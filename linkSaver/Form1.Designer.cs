a2da92f45698346145212e7ef91cffac21fd95ea
Joshua Conklin <jconklin@escautomation.com>
2020-09-03 12:44:50 -0600

    Add project files.

---
+++ linkSaver/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace linkSaver
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstLinks = new System.Windows.Forms.ListBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.cmdAddLink = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cmdGo = new System.Windows.Forms.Button();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstLinks
+            // 
+            this.lstLinks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstLinks.FormattingEnabled = true;
+            this.lstLinks.ItemHeight = 20;
+            this.lstLinks.Location = new System.Drawing.Point(78, 85);
+            this.lstLinks.Name = "lstLinks";
+            this.lstLinks.Size = new System.Drawing.Size(314, 284);
+            this.lstLinks.TabIndex = 0;
+            this.lstLinks.SelectedIndexChanged += new System.EventHandler(this.lstLinks_SelectedIndexChanged);
+            this.lstLinks.DoubleClick += new System.EventHandler(this.lstLinks_DoubleClick);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtURL.Location = new System.Drawing.Point(78, 12);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(314, 29);
+            this.txtURL.TabIndex = 1;
+            // 
+            // cmdAddLink
+            // 
+            this.cmdAddLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAddLink.Location = new System.Drawing.Point(398, 12);
+            this.cmdAddLink.Name = "cmdAddLink";
+            this.cmdAddLink.Size = new System.Drawing.Size(75, 66);
+            this.cmdAddLink.TabIndex = 2;
+            this.cmdAddLink.Text = "Add Link";
+            this.cmdAddLink.UseVisualStyleBackColor = true;
+            this.cmdAddLink.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(78, 49);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(314, 29);
+            this.txtName.TabIndex = 4;
+            // 
+            // cmdGo
+            // 
+            this.cmdGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdGo.Location = new System.Drawing.Point(317, 383);
+            this.cmdGo.Name = "cmdGo";
+            this.cmdGo.Size = new System.Drawing.Size(75, 66);
+            this.cmdGo.TabIndex = 6;
+            this.cmdGo.Text = "Go";
+            this.cmdGo.UseVisualStyleBackColor = true;
+            this.cmdGo.Click += new System.EventHandler(this.cmdGo_Click);
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdDelete.Location = new System.Drawing.Point(78, 383);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(75, 66);
+            this.cmdDelete.TabIndex = 8;
+            this.cmdDelete.Text = "Delete";
+            this.cmdDelete.UseVisualStyleBackColor = true;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(159, 387);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Save Count";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCount
+            // 
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(159, 419);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(149, 24);
+            this.lblCount.TabIndex = 10;
+            this.lblCount.Text = "Save Count";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 457);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmdDelete);
+            this.Controls.Add(this.cmdGo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdAddLink);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.lstLinks);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "linkSaver";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstLinks;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button cmdAddLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button cmdGo;
+        private System.Windows.Forms.Button cmdDelete;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ linkSaver/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace linkSaver
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addLink(txtURL.Text, txtName.Text);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lstLinks.DataSource = Properties.Settings.Default.listboxItems.Cast<string>().ToArray();
+                lstLinks.SelectedIndex = -1;
+                lblCount.Text = (lstLinks.Items.Count).ToString();
+            }
+
+            catch
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+
+        private void lstLinks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblCount.Text = (lstLinks.Items.Count ).ToString();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+        private void addLink(string targetURL, string targetName)
+        {
+            string setting = targetName + "," + targetURL;
+            Properties.Settings.Default.listboxItems.Add(setting);
+
+            lstLinks.DataSource = Properties.Settings.Default.listboxItems.Cast<string>().ToArray();
+            lstLinks.SelectedIndex = -1;
+            txtName.Text = "";
+            txtURL.Text = "";
+        }
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+            deleteSelectedItem(lstLinks);
+        }
+        private void deleteSelectedItem(ListBox targetListbox)
+        {
+            Properties.Settings.Default.listboxItems.Remove(lstLinks.GetItemText(lstLinks.SelectedItem));
+            targetListbox.Items.Remove(lstLinks.SelectedItems);
+            targetListbox.DataSource = null;
+            targetListbox.DataSource = Properties.Settings.Default.listboxItems.Cast<string>().ToArray();
+            targetListbox.Refresh();
+        }
+        private void gotoURL()
+        {
+            string[] parseURL = lstLinks.GetItemText(lstLinks.SelectedItem).Split(',');
+            Process.Start("chrome.exe", parseURL[1]);
+        }
+        private void cmdGo_Click(object sender, EventArgs e)
+        {
+            gotoURL();
+        }
+
+        private void lstLinks_DoubleClick(object sender, EventArgs e)
+        {
+            gotoURL();
+        }
+    }
+}
